fcbe6c9a4ee84eabb5ffbd243c87244dc92800cc
Anupat Kaewmee <anupat.k64@rsu.ac.th>
2024-06-21 12:53:53 +0700

    add migration script

---
+++ src/migrations/customer_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.customer (customer_id,email,"password",citizen_id,fname,lname,telephone,address_line_1,address_line_2,package) VALUES
+	 ('Test','Test@test.test','P@ssw0rdVF','1111111111111','TestFname','TestLname','1111111111','TestAddress',NULL,'vip');
